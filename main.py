fe5d9b7603e2be60e1c77216e6ba0201d5f2def7
liuyibo <1299502716@qq.com>
2023-01-07 16:27:56 +0800

    new file:   .gitignore 	modified:   app/models/__pycache__/models.cpython-39.pyc 	modified:   app/views/__pycache__/views.cpython-39.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    print(app.url_map)
+    # print(app.url_map)
 
     # execute app
     app.run(host="localhost", port="1234", debug=True)
